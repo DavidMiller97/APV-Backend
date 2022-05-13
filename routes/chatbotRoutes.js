2217406e1e0f48ede80f3f92339605004aee2952
David Latapi <david211910@gmail.com>
2022-05-13 15:24:25 -0500

    Errores dialog

--- routes/chatbotRoutes.js
+++ routes/chatbotRoutes.js
@@ -84,6 +84,7 @@ async function receivedMessage(event) {
   var recipientID = event.recipient.id;
   var timeOfMessage = event.timestamp;
   var message = event.message;
+  console.log(dialogflow);
   console.log(
     "Received message for user %d and page %d at %d with message:",
     senderId,
